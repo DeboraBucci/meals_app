4349f348939b6105126f51ea65aec6128684384b
Debora Bucci <dbucci@protonmail.com>
2025-01-07 21:48:25 -0300

    add message to the FavoritesScreen for when not having any favorite meal

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -2,6 +2,7 @@ import MealsList from "../components/MealsList/MealsList";
 import { useContext } from "react";
 import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
+import { StyleSheet, Text, View } from "react-native";
 
 const FavoritesScreen = () => {
   const favoriteMealsContext = useContext(FavoritesContext);
@@ -10,7 +11,29 @@ const FavoritesScreen = () => {
     favoriteMealsContext.ids.includes(meal.id)
   );
 
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You don't have any favorite meals yet!</Text>
+      </View>
+    );
+  }
+
   return <MealsList items={favoriteMeals} />;
 };
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
